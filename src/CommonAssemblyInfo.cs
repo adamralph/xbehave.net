fe001a30b46da84872e99d9c922e119e39d8f250
Adam Ralph <adam@adamralph.com>
2012-02-28 00:06:16 +0100

    incremented to 0.8.0-a cleared release notes

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyInformationalVersion("0.8.0-a")]
