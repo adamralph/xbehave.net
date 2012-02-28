b59190a96e196e9f437ca2568356d6ae5e040c1d
Adam Ralph <adam@adamralph.com>
2012-02-28 20:39:48 +0100

    moved SubSpecGWT issue types from Samples to Issues\Old

--- src/test/Xbehave.Issues/Old/Issue2.cs
+++ src/test/Xbehave.Issues/Old/Issue2.cs
@@ -2,7 +2,7 @@
 //  Copyright (c) Adam Ralph. All rights reserved.
 // </copyright>
 
-namespace Xbehave.Samples
+namespace Xbehave.Issues.Old
 {
     using FluentAssertions;
 
---
+++ src/test/Xbehave.Issues/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xbehave.Issues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Xbehave.Issues")]
+
+[assembly: ComVisible(false)]
