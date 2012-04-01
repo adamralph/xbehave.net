87e02ec7537a53ed530262a1f10897db7ea4e188
Adam Ralph <adam@adamralph.com>
2012-04-01 16:08:48 +0100

    fixed SpecificationPrimitiveFacts for StyleCop

--- src/test/Xbehave.Test/SpecificationPrimitiveFacts.cs
+++ src/test/Xbehave.Test/SpecificationPrimitiveFacts.cs
@@ -1,86 +1,25 @@
-﻿using System;
-using Xunit;
-using Xbehave;
-using System.Threading;
+﻿// <copyright file="SpecificationPrimitiveFacts.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
 
-//public class SpecificationPrimitiveFacts
-//{
-//    [Fact]
-//    public void CreatePrimitive_WithNullAction_Throws()
-//    {
-//        Assert.Throws<ArgumentNullException>( () => new SpecificationPrimitive<Action>( "", null ) );
-//    }
+namespace Xbehave.Test
+{
+    using System;
+    using Xbehave;
+    using Xunit;
 
-//    [Fact]
-//    public void CreatePrimitive_WithNullMessage_Throws()
-//    {
-//        Assert.Throws<ArgumentNullException>( () => new SpecificationPrimitive<Action>( null, () => { } ) );
-//    }
+    public class SpecificationPrimitiveFacts
+    {
+        [Fact]
+        public void CreatePrimitive_WithNullAction_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Step<Action>("foo", null));
+        }
 
-//    [Fact]
-//    public void Execute_Action_WithTimeout_FailsCorrectly()
-//    {
-//        SpecificationPrimitive<Action> sut = new SpecificationPrimitive<Action>( "", () => Thread.Sleep( 1000 ) );
-
-//        sut.WithTimeout( 10 );
-
-//        Assert.Throws<Xunit.Sdk.TimeoutException>( () => sut.Execute() );
-//    }
-
-//    [Fact]
-//    public void Execute_Action_Timeout_HappyPath()
-//    {
-//        SpecificationPrimitive<Action> sut = new SpecificationPrimitive<Action>( "", () => { } );
-
-//        sut.WithTimeout( 10 );
-
-//        Assert.DoesNotThrow( () => sut.Execute() );
-//    }
-
-//    [Fact]
-//    public void Execute_Action_NoTimeout_HappyPath()
-//    {
-//        SpecificationPrimitive<Action> sut = new SpecificationPrimitive<Action>( "", () => { } );
-
-//        Assert.DoesNotThrow( () => sut.Execute() );
-//    }
-
-//    [Fact]
-//    public void Execute_ContextDelegate_WithTimeout_FailsCorrectly()
-//    {
-//        SpecificationPrimitive<ContextDelegate> sut = new SpecificationPrimitive<ContextDelegate>( "",
-//            () =>
-//            {
-//                Thread.Sleep( 20 );
-//                return DisposableAction.None;
-//            } );
-
-//        sut.WithTimeout( 10 );
-
-//        Assert.Throws<Xunit.Sdk.TimeoutException>( () => sut.Execute() );
-//    }
-
-//    [Fact]
-//    public void Execute_ContextDelegate_WithTimeout_HappyPath()
-//    {
-//        SpecificationPrimitive<ContextDelegate> sut = new SpecificationPrimitive<ContextDelegate>( "", () => DisposableAction.None );
-
-//        sut.WithTimeout( 10 );
-
-//        var result = default( IDisposable );
-//        Assert.DoesNotThrow( () => result = sut.Execute() );
-
-//        Assert.Equal( DisposableAction.None, result );
-//    }
-
-//    [Fact]
-//    public void Execute_ContextDelegate_NoTimeout_HappyPath()
-//    {
-//        SpecificationPrimitive<ContextDelegate> sut = new SpecificationPrimitive<ContextDelegate>( "", () => DisposableAction.None );
-
-//        var result = default( IDisposable );
-//        Assert.DoesNotThrow( () => result = sut.Execute() );
-
-//        Assert.Equal( DisposableAction.None, result );
-//    }
-//}
+        [Fact]
+        public void CreatePrimitive_WithNullMessage_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Step<Action>(null, () => { }));
+        }
+    }
+}
