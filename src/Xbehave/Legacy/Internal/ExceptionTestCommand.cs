f8bd68fae6a833ec3f96d3ab3278d9c0109b1477
Adam Ralph <adam@adamralph.com>
2012-04-06 00:21:30 +0200

    simplified ExceptionTestCommand constructor renamed ScenarioContext.exceptions to throwActions improved ScenarioContext exception messages

--- src/Xbehave/Legacy/Internal/ExceptionTestCommand.cs
+++ src/Xbehave/Legacy/Internal/ExceptionTestCommand.cs
@@ -4,12 +4,13 @@
 
 namespace Xbehave.Legacy
 {
+    using System;
     using Xunit.Sdk;
 
     internal class ExceptionTestCommand : ActionTestCommand
     {
-        public ExceptionTestCommand(IMethodInfo method, Xunit.Assert.ThrowsDelegate action)
-            : base(method, null, 0, () => action())
+        public ExceptionTestCommand(IMethodInfo method, Action action)
+            : base(method, null, 0, action)
         {
         }
 
