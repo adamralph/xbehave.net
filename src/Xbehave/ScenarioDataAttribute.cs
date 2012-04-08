6ac2d1ae8882b7cb89cd153975795e06f441a9ef
Adam Ralph <adam@adamralph.com>
2012-04-08 12:58:02 +0200

    Backed out changeset: f75838f319fc

--- src/Xbehave/ScenarioDataAttribute.cs
+++ src/Xbehave/ScenarioDataAttribute.cs
@@ -24,5 +24,21 @@ namespace Xbehave
     [SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes", Justification = "Designed for extensibility.")]
     public class ScenarioDataAttribute : InlineDataAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScenarioDataAttribute"/> class.
+        /// This attribute is designed as a synonym of <see cref="Xunit.Extensions.InlineDataAttribute"/>,
+        /// which is the most commonly used data attribute, but you can also use any type of attribute derived from
+        /// <see cref="Xunit.Extensions.DataAttribute"/> to provide a data source for a scenario.
+        /// E.g. <see cref="Xunit.Extensions.ClassDataAttribute"/>,
+        /// <see cref="Xunit.Extensions.OleDbDataAttribute"/>,
+        /// <see cref="Xunit.Extensions.SqlServerDataAttribute"/>,
+        /// <see cref="Xunit.Extensions.ExcelDataAttribute"/> or
+        /// <see cref="Xunit.Extensions.PropertyDataAttribute"/>.
+        /// </summary>
+        /// <param name="dataValues">The data values to pass to the scenario.</param>
+        public ScenarioDataAttribute(params object[] dataValues)
+            : base(dataValues)
+        {
+        }
     }
 }
