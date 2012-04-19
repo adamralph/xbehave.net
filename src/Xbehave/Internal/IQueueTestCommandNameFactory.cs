543e36c5f6e30dbfaa2a2706ed401a450915755b
Adam Ralph <adam@adamralph.com>
2012-04-19 08:17:46 +0200

    added queue factory skeletons

---
+++ src/Xbehave/Internal/IQueueTestCommandNameFactory.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IQueueTestCommandNameFactory.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Internal
+{
+    using System.Collections.Generic;
+
+    internal interface IQueueTestCommandNameFactory
+    {
+        string CreateSharedContextCreationName(Queue<Step> steps);
+
+        string CreateSharedContextTestName(Queue<Step> contextSteps, Queue<Step> testSteps);
+
+        string CreateSharedContextDisposalName(Queue<Step> steps);
+
+        string CreateIsolatedContextTestName(Queue<Step> steps);
+
+        string CreateSkippedTestName(Queue<Step> steps);
+    }
+}
---
+++ src/Xbehave/Internal/QueueTestCommandFactory.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="QueueTestCommandFactory.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Internal
+{
+    using System.Collections.Generic;
+    using Xunit.Sdk;
+
+    internal class QueueTestCommandFactory : IQueueTestCommandFactory
+    {
+        private readonly IQueueTestCommandNameFactory nameFactory;
+
+        public QueueTestCommandFactory(IQueueTestCommandNameFactory nameFactory)
+        {
+            this.nameFactory = nameFactory;
+        }
+
+        public IEnumerable<ITestCommand> Create(Queue<Step> steps, IMethodInfo method)
+        {
+            // TODO: actually write some code
+            return new ITestCommand[0];
+        }
+    }
+}
