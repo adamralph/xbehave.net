b77c2ba694bdfa79e4c8f93a1e68565de63206b6
Adam Ralph <adam@adamralph.com>
2012-04-20 23:23:30 +0200

    added demo of multiple givens and whens

---
+++ src/issues/Xbehave.Issues/MultipleGivensAndWhens.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="MultipleGivensAndWhens.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Issues
+{
+    using System;
+    using FluentAssertions;
+
+    public class MultipleGivensAndWhens
+    {
+        // 2 failures with 1 x 1 disposal
+        [Scenario]
+        public static void MultipleWithActionDisposables()
+        {
+            var disposable0 = default(Disposable);
+            var disposable1 = default(Disposable);
+
+            "Given a disposable,"
+                .Given(
+                    () => disposable0 = new Disposable(),
+                    () => disposable0.Dispose());
+
+            "and another disposable"
+                .Given(
+                    () => disposable1 = new Disposable(),
+                    () => disposable1.Dispose());
+
+            "when using the first disposable"
+                .When(() => disposable0.Use());
+
+            "and using the second disposable"
+                .When(() => disposable1.Use());
+
+            _.ThenInIsolation(() => true.Should().Be(false));
+            _.ThenInIsolation(() => true.Should().Be(false));
+        }
+
+        public sealed class Disposable : IDisposable
+        {
+            public Disposable()
+            {
+                Console.WriteLine("CREATED");
+            }
+
+            public void Use()
+            {
+                Console.WriteLine("USED");
+            }
+
+            public void Dispose()
+            {
+                Console.WriteLine("DISPOSED");
+            }
+        }
+    }
+}
