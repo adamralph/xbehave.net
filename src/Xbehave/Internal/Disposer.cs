f9fc8f3a7bb93bfb00cc45749a60298df1f87a5c
Adam Ralph <adam@adamralph.com>
2012-04-21 10:13:39 +0200

    code analysis suppression

--- src/Xbehave/Internal/Disposer.cs
+++ src/Xbehave/Internal/Disposer.cs
@@ -6,11 +6,13 @@ namespace Xbehave.Internal
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
 
     internal class Disposer : IDisposer
     {
         // NOTE (aralph1): in practice we could accept IEnumerable since a Stack enumerates in a reverse order as required, but this behaviour is undocumented
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Emulating behaviour of nested using blocks")]
         public void Dispose(Stack<IDisposable> disposables)
         {
             Exception lastException = null;
