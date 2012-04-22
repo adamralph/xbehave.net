76c24d84ce5d09e9e17a0cd32872aaa1e2e46b32
Adam Ralph <adam@adamralph.com>
2012-04-22 14:44:00 +0200

    added Test build task

--- src/build-tasks.ps1
+++ src/build-tasks.ps1
@@ -1,14 +1,21 @@
 Properties {
     $MSBuildConfiguration="Release"
+    $XunitConsole="..\packages\xunit.runners.1.9.0.1566\tools\xunit.console.clr4.exe"
+    
     Write-Host "MSBuildConfiguration = $MSBuildConfiguration"
+    Write-Host "XunitConsole = $XunitConsole"
 }
 
-Task Default -depends Build
+Task Default -depends Test
+
+Task Test -depends Build {
+    Exec { .$XunitConsole "test\Xbehave.Test\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "test\Xbehave.Test\bin\Debug\XBehave.Test.Results.Xml" }
+}
 
 Task Build -depends Clean {
-    Exec { msbuild "Xbehave.sln" /p:Configuration=$MSBuildConfiguration }
+    Exec { msbuild "Xbehave.sln" /t:Build /p:Configuration=$MSBuildConfiguration }
 }
 
 Task Clean {
-    Exec { msbuild "Xbehave.sln" /t:clean /p:Configuration=$MSBuildConfiguration }
+    Exec { msbuild "Xbehave.sln" /t:Clean /p:Configuration=$MSBuildConfiguration }
 }
