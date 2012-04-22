f5a16e6530531a79b531d2c819c3211bbc96f5dd
Adam Ralph <adam@adamralph.com>
2012-04-22 19:32:27 +0200

    added Package build task

--- src/build-tasks.ps1
+++ src/build-tasks.ps1
@@ -1,10 +1,36 @@
 Properties {
     $MSBuildConfiguration="Release"
+    $NuGetConsole="..\packages\NuGet.CommandLine.1.7.0\tools\NuGet.exe"
     $XunitConsole35="..\packages\xunit.runners.1.9.0.1566\tools\xunit.console.exe"
     $XunitConsole40="..\packages\xunit.runners.1.9.0.1566\tools\xunit.console.clr4.exe"
 }
 
-Task Default -depends Test
+Task Default -depends Package
+
+Task Package -depends Test {
+    if (Test-Path bin)
+    {
+        rd bin -Force -Recurse
+    }
+    
+    mkdir bin\lib\net35
+    mkdir bin\lib\net40
+    
+    copy "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.dll" "bin\lib\net35"
+    copy "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.dll" "bin\lib\net40"
+    
+    if (Test-Path "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml")
+    {
+        copy "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml" "bin\lib\net35"
+    }
+    
+    if (Test-Path "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml")
+    {    
+        copy "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml" "bin\lib\net40"
+    }
+    
+    Exec { .$NuGetConsole pack "Xbehave.nuspec" -BasePath bin -OutputDirectory bin }
+}
 
 Task Test -depends Build {
     Exec { .$XunitConsole35 "test\Xbehave.Test.Net35\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.xml" /html "test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.html" }
@@ -21,6 +47,7 @@ Task Clean -depends ResolveVariables {
 
 Task ResolveVariables {
     Write-Host "MSBuildConfiguration = $MSBuildConfiguration"
+    Write-Host "NuGetConsole = $NuGetConsole"
     Write-Host "XunitConsole35 = $XunitConsole35"
     Write-Host "XunitConsole40 = $XunitConsole40"
 }
