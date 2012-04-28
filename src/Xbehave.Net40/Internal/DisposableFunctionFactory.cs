f0a2849c3cb9179faf4d1628177547d9f491de7c
Adam Ralph <adam@adamralph.com>
2012-04-28 21:39:29 +0200

    moved step creation responsibility back into StringExtensions moved Func<IDisposable> creation into DisposableFunctionFactory

---
+++ src/Xbehave.Net40/Internal/DisposableFunctionFactory.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="DisposableFunctionFactory.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Internal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xbehave.Infra;
+
+    internal static class DisposableFunctionFactory
+    {
+        public static Func<IDisposable> Create(Func<IDisposable> source)
+        {
+            return source;
+        }
+
+        public static Func<IDisposable> Create(Action source, Action dispose)
+        {
+            return source == null ? null : new Func<IDisposable>(() =>
+                {
+                    source();
+                    return new Disposable(dispose);
+                });
+        }
+
+        public static Func<IDisposable> Create(Func<IEnumerable<IDisposable>> source)
+        {
+            return source == null ? null : new Func<IDisposable>(() => new Disposable(source().Reverse()));
+        }
+
+        public static Func<IDisposable> Create(Action source)
+        {
+            return source == null ? null : new Func<IDisposable>(() =>
+                {
+                    source();
+                    return default(IDisposable);
+                });
+        }
+    }
+}
