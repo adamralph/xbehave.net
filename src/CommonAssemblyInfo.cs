409400910933490b9b8a51affa0d8d18469feb3a
Adam Ralph <adam@adamralph.com>
2012-05-02 22:50:34 +0200

    incremented to 0.10.0-alpha03

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.10.0.0")]
 [assembly: AssemblyFileVersion("0.10.0.0")]
-[assembly: AssemblyInformationalVersion("0.10.0-alpha02")]
+[assembly: AssemblyInformationalVersion("0.10.0-alpha03")]
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
