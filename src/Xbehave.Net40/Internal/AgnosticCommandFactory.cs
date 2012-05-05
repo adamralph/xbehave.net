fc8010c360196d919553f2053961232349f4b6ff
Adam Ralph <adam@adamralph.com>
2012-05-05 11:13:54 +0200

    added agnostic command factory skeleton

---
+++ src/Xbehave.Net40/Internal/AgnosticCommandFactory.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="AgnosticCommandFactory.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Internal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using Xbehave.Infra;
+    using Xunit.Sdk;
+
+    internal class AgnosticCommandFactory : IAgnosticCommandFactory
+    {
+        private readonly IDisposer disposer;
+
+        public AgnosticCommandFactory(IDisposer disposer)
+        {
+            this.disposer = disposer;
+        }
+
+        public IEnumerable<ITestCommand> Create(IEnumerable<Step> steps, IMethodInfo method)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Xbehave.Net40/Internal/IAgnosticCommandFactory.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IAgnosticCommandFactory.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Internal
+{
+    using System.Collections.Generic;
+    using Xunit.Sdk;
+
+    internal interface IAgnosticCommandFactory
+    {
+        IEnumerable<ITestCommand> Create(IEnumerable<Step> steps, IMethodInfo method);
+    }
+}
