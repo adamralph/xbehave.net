9a5f7d9cf72b86f388933fbb87516120bb784c35
Adam Ralph <adam@adamralph.com>
2012-05-06 14:53:06 +0200

    changed command name format to "{call} [{context ordinal}, {test ordinal}] {command name}"

--- src/Xbehave.Net40/Internal/CommandBase.cs
+++ src/Xbehave.Net40/Internal/CommandBase.cs
@@ -5,7 +5,6 @@
 namespace Xbehave.Internal
 {
     using System.Globalization;
-    using System.Linq;
     using Xunit.Sdk;
 
     internal abstract class CommandBase : TestCommand
@@ -14,15 +13,13 @@ namespace Xbehave.Internal
             : base(call.Method, commandName, MethodUtility.GetTimeoutParameter(call.Method))
         {
             var provider = CultureInfo.InvariantCulture;
-            var tokens = new[]
-                {
-                    call.ToString(),
-                    contextOrdinal.HasValue ? string.Format(provider, "context {0} ", contextOrdinal.Value.ToString("D2", provider)) : null,
-                    string.Format(provider, "test {0} ", commandOrdinal.ToString("D2", provider)),
-                    commandName,
-                };
-
-            this.DisplayName = string.Join(", ", tokens.Where(token => token != null).ToArray());
+            this.DisplayName = string.Format(
+                provider,
+                "{0} [{1}{2}] {3}",
+                call.ToString(),
+                contextOrdinal.HasValue ? string.Format(provider, "context {0}, ", contextOrdinal.Value.ToString("D2", provider)) : null,
+                string.Format(provider, "test {0}", commandOrdinal.ToString("D2", provider)),
+                commandName);
         }
     }
 }
