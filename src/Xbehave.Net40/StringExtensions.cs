445d4a5cf55fa42fb37ca67cf72398c103ea8817
Adam Ralph <adam@adamralph.com>
2012-05-06 17:18:13 +0200

    add _ overloads for disposables

--- src/Xbehave.Net40/StringExtensions.cs
+++ src/Xbehave.Net40/StringExtensions.cs
@@ -110,6 +110,26 @@ namespace Xbehave
         }
 
         /// <summary>
+        /// Defines a step in the current scenario which returns a resource which will be disposed after all remaining steps have been executed.
+        /// </summary>
+        /// <param name="message">A message describing the step.</param>
+        /// <param name="step">The function that will perform the step and return the disposable resource.</param>
+        /// <param name="inIsolation">if set to <c>true</c> an isolated context will be created containing this step and a copy of all preceding steps.</param>
+        /// <param name="skip">
+        /// Marks the step so that it will not be executed and provides the reason.
+        /// This is the equivalent of <see cref="Xunit.FactAttribute.Skip"/>.
+        /// E.g. <code>[Fact(Skip = "Work in progress.")]</code>.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="IStepDefinition"/>.
+        /// </returns>
+        [CLSCompliant(false)]
+        public static IStepDefinition _(this string message, Func<IDisposable> step, bool inIsolation = false, string skip = null)
+        {
+            return new StepDefinition(CurrentThread.Scenario.Enqueue(new Step(message, DisposableFunctionFactory.Create(step), inIsolation, skip)));
+        }
+
+        /// <summary>
         /// Defines a step in the current scenario.
         /// </summary>
         /// <param name="message">A message describing the step.</param>
@@ -126,7 +146,46 @@ namespace Xbehave
         [CLSCompliant(false)]
         public static IStepDefinition _(this string message, Action step, bool inIsolation = false, string skip = null)
         {
-            return new StepDefinition(CurrentThread.Scenario.Enqueue(new Step(message, DisposableFunctionFactory.Create(step), inIsolation, skip)));
+            return _(message, DisposableFunctionFactory.Create(step), inIsolation, skip);
+        }
+
+        /// <summary>
+        /// Defines a step in the current scenario which returns a collection of resources which will be disposed after all remaining steps have been executed.
+        /// </summary>
+        /// <param name="message">A message describing the step.</param>
+        /// <param name="step">The function that will perform the step and return the disposable resources.</param>
+        /// <param name="inIsolation">if set to <c>true</c> an isolated context will be created containing this step and a copy of all preceding steps.</param>
+        /// <param name="skip">
+        /// Marks the step so that it will not be executed and provides the reason.
+        /// This is the equivalent of <see cref="Xunit.FactAttribute.Skip"/>.
+        /// E.g. <code>[Fact(Skip = "Work in progress.")]</code>.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="IStepDefinition"/>.
+        /// </returns>
+        [CLSCompliant(false)]
+        public static IStepDefinition _(this string message,  Func<IEnumerable<IDisposable>> step, bool inIsolation = false, string skip = null)
+        {
+            return _(message, DisposableFunctionFactory.Create(step), inIsolation, skip);
+        }
+
+        /// <summary>
+        /// Defines a step in the current scenario which returns a resource which will be disposed after all remaining steps have been executed.
+        /// </summary>
+        /// <param name="message">A message describing the step.</param>
+        /// <param name="step">The function that will perform the step and return the disposable resource.</param>
+        /// <param name="dispose">The action that will dispose the resource.</param>
+        /// <param name="inIsolation">if set to <c>true</c> an isolated context will be created containing this step and a copy of all preceding steps.</param>
+        /// <param name="skip">Marks the step so that it will not be executed and provides the reason.
+        /// This is the equivalent of <see cref="Xunit.FactAttribute.Skip"/>.
+        /// E.g. <code>[Fact(Skip = "Work in progress.")]</code>.</param>
+        /// <returns>
+        /// An instance of <see cref="IStepDefinition"/>.
+        /// </returns>
+        [CLSCompliant(false)]
+        public static IStepDefinition _(this string message, Action step, Action dispose, bool inIsolation = false, string skip = null)
+        {
+            return _(message, DisposableFunctionFactory.Create(step, dispose), inIsolation, skip);
         }
     }
 }
