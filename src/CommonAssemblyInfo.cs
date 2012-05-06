31240140f0fbc5483c7b85a484f06116e511a042
Adam Ralph <adam@adamralph.com>
2012-05-06 22:32:58 +0200

    increment to 0.10.0-alpha04

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.10.0.0")]
 [assembly: AssemblyFileVersion("0.10.0.0")]
-[assembly: AssemblyInformationalVersion("0.10.0-alpha03")]
+[assembly: AssemblyInformationalVersion("0.10.0-alpha04")]
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
