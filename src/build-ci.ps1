cd680478d0eec32604ebda03f35855f5e5cf75b4
Adam Ralph <adam@adamralph.com>
2012-05-10 20:58:40 +0200

    re # 7 added setting of location to current file folder in each build script

--- src/build-ci.ps1
+++ src/build-ci.ps1
@@ -1,3 +1,4 @@
+Split-Path -Path $MyInvocation.MyCommand.Definition -Parent | Set-Location
 Import-Module ..\packages\psake.4.1.0\tools\psake.psm1 -force
 Invoke-psake .\build-tasks.ps1 -properties  @{"MSBuildConfiguration"="CI"}
 Remove-Module psake
