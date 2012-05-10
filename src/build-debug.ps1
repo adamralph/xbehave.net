4648ff1ca7e99400945e47e3f03a397656fbdf11
Adam Ralph <adam@adamralph.com>
2012-05-10 21:26:05 +0200

    Backed out changeset: 421949887f18

--- src/build-debug.ps1
+++ src/build-debug.ps1
@@ -1,4 +1,3 @@
-Split-Path -Path $MyInvocation.MyCommand.Definition -Parent | Set-Location
 Import-Module ..\packages\psake.4.1.0\tools\psake.psm1 -force
 Invoke-psake .\build-tasks.ps1 -properties  @{"MSBuildConfiguration"="Debug"}
 Remove-Module psake
