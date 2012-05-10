299532dd9cd91b3289b8bed15e807659797dfd59
Adam Ralph <adam@adamralph.com>
2012-05-10 21:55:25 +0200

    made PS scripts $PWD agnostic

--- src/build-debug.ps1
+++ src/build-debug.ps1
@@ -1,3 +1,4 @@
-Import-Module ..\packages\psake.4.1.0\tools\psake.psm1 -force
-Invoke-psake .\build-tasks.ps1 -properties  @{"MSBuildConfiguration"="Debug"}
+$ThisDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+Import-Module $ThisDir\..\packages\psake.4.1.0\tools\psake.psm1 -force
+Invoke-psake $ThisDir\build-tasks.ps1 -properties  @{"MSBuildConfiguration"="Debug"}
 Remove-Module psake
--- src/build-tasks.ps1
+++ src/build-tasks.ps1
@@ -1,4 +1,5 @@
 Properties {
+    $ThisDir = Split-Path $psake.build_script_file
     $MSBuildConfiguration="Release"
     $NuGetConsole="..\packages\NuGet.CommandLine.1.7.0\tools\NuGet.exe"
     $XunitConsole35="..\packages\xunit.runners.1.9.0.1566\tools\xunit.console.exe"
@@ -8,44 +9,45 @@ Properties {
 Task Default -depends Package
 
 Task Package -depends Test {
-    if (Test-Path bin)
+    if (Test-Path "$ThisDir\bin")
     {
-        rd bin -Force -Recurse
+        rd "$ThisDir\bin" -Force -Recurse
     }
     
-    mkdir bin\lib\net35
-    mkdir bin\lib\net40
+    mkdir "$ThisDir\bin\lib\net35"
+    mkdir "$ThisDir\bin\lib\net40"
     
-    copy "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.dll" "bin\lib\net35"
-    copy "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.dll" "bin\lib\net40"
+    copy "$ThisDir\Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.dll" "$ThisDir\bin\lib\net35"
+    copy "$ThisDir\Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.dll" "$ThisDir\bin\lib\net40"
     
-    if (Test-Path "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml")
+    if (Test-Path "$ThisDir\Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml")
     {
-        copy "Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml" "bin\lib\net35"
+        copy "$ThisDir\Xbehave.Net35\bin\$MSBuildConfiguration\Xbehave.xml" "$ThisDir\bin\lib\net35"
     }
     
-    if (Test-Path "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml")
+    if (Test-Path "$ThisDir\Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml")
     {    
-        copy "Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml" "bin\lib\net40"
+        copy "$ThisDir\Xbehave.Net40\bin\$MSBuildConfiguration\Xbehave.xml" "$ThisDir\bin\lib\net40"
     }
     
-    Exec { .$NuGetConsole pack "Xbehave.nuspec" -BasePath bin -OutputDirectory bin }
+    Exec { .$NuGetConsole pack "Xbehave.nuspec" -BasePath "$ThisDir\bin" -OutputDirectory "$ThisDir\bin" }
 }
 
 Task Test -depends Build {
-    Exec { .$XunitConsole35 "test\Xbehave.Test.Net35\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.xml" /html "test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.html" }
-    Exec { .$XunitConsole40 "test\Xbehave.Test.Net40\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "test\Xbehave.Test.Net40\bin\Debug\XBehave.Test.Results.xml" /html "test\Xbehave.Test.Net40\bin\Debug\XBehave.Test.Results.html" }
+    Exec { .$XunitConsole35 "$ThisDir\test\Xbehave.Test.Net35\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "$ThisDir\test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.xml" /html "$ThisDir\test\Xbehave.Test.Net35\bin\Debug\XBehave.Test.Results.html" }
+    Exec { .$XunitConsole40 "$ThisDir\test\Xbehave.Test.Net40\bin\Debug\Xbehave.Test.dll" /noshadow /nunit "$ThisDir\test\Xbehave.Test.Net40\bin\Debug\XBehave.Test.Results.xml" /html "$ThisDir\test\Xbehave.Test.Net40\bin\Debug\XBehave.Test.Results.html" }
 }
 
 Task Build -depends Clean {
-    Exec { msbuild "Xbehave.sln" /t:Build /p:Configuration=$MSBuildConfiguration }
+    Exec { msbuild "$ThisDir\Xbehave.sln" /t:Build /p:Configuration=$MSBuildConfiguration }
 }
 
 Task Clean -depends ResolveVariables {
-    Exec { msbuild "Xbehave.sln" /t:Clean /p:Configuration=$MSBuildConfiguration }
+    Exec { msbuild "$ThisDir\Xbehave.sln" /t:Clean /p:Configuration=$MSBuildConfiguration }
 }
 
 Task ResolveVariables {
+    Write-Host "ThisDir = $ThisDir"
     Write-Host "MSBuildConfiguration = $MSBuildConfiguration"
     Write-Host "NuGetConsole = $NuGetConsole"
     Write-Host "XunitConsole35 = $XunitConsole35"
