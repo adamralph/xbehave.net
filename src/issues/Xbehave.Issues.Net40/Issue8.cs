a6405f508844d08883a218ca2de8dccb6cfa4487
Adam Ralph <adam@adamralph.com>
2012-05-14 22:09:47 +0200

    close #8 add ExceptionCommand add generation of ExceptionCommand if exception thrown during command generation updated release notes

---
+++ src/issues/Xbehave.Issues.Net40/Issue8.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Issue8.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Issues.Old
+{
+    using Xbehave;
+
+    /// <summary>
+    /// https://bitbucket.org/adamralph/xbehave.net/issue/8/fix-infinite-loops-when-running-from-tdnet
+    /// </summary>
+    public class Issue8
+    {
+        [Scenario]
+        public static void ThrowsException()
+        {
+            throw new System.Exception();
+        }
+    }
+}
