643911410aa059c4cf97b14b05a0a5042e8bb392
Adam Ralph <adam@adamralph.com>
2012-06-04 21:05:05 +0200

    added null examples to StringExtensionsFeature.MergingUsingOrdinalIgnoreCaseComparison added StringExtensionsFeature.AttemptingToFormatUsingInvariantCulture

--- src/test/Xbehave.Test.Net40/Infra/StringExtensionsFeature.cs
+++ src/test/Xbehave.Test.Net40/Infra/StringExtensionsFeature.cs
@@ -30,6 +30,8 @@ namespace Xbehave.Test.Infra
         }
 
         [Scenario]
+        [Example(null, "foo", "foo")]
+        [Example("foo", null, "foo")]
         [Example("Given ", "a foo", "Given a foo")]
         [Example("Given ", "given a foo", "given a foo")]
         [Example("Given ", "n foos", "Given n foos")]
@@ -45,6 +47,26 @@ namespace Xbehave.Test.Infra
 
             "When merging using ordinal ignore case comparison"
                 .When(() => result = originalText.MergeOrdinalIgnoreCase(textToMerge));
+
+            "Then the result should be \"{2}\""
+                .Then(() => result.Should().Be(expectedResult));
+        }
+
+        [Scenario]
+        [Example("a{0}c", "b", "abc")]
+        [Example("a{1}c", "b", "a{1}c")]
+        public static void AttemptingToFormatUsingInvariantCulture(string format, string arg, string expectedResult)
+        {
+            var result = default(string);
+
+            "Given format \"{0}\""
+                .Given(() => { });
+
+            "And arg \"{1}\""
+                .And(() => { });
+
+            "When attempting to format using the invariant culture"
+                .When(() => result = format.AttemptFormatInvariantCulture(arg));
 
             "Then the result should be \"{2}\""
                 .Then(() => result.Should().Be(expectedResult));
