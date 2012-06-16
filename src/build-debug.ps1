850c7973467e2f1697e4168b202c4dc5433ee493
Adam Ralph <adam@adamralph.com>
2012-06-16 13:07:04 +0200

    upgrade to NuGet.CommandLine.1.8.40002 upgrade to psake.4.2.0.1

--- src/build-debug.ps1
+++ src/build-debug.ps1
@@ -1,4 +1,4 @@
 $ThisDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
-Import-Module $ThisDir\..\packages\psake.4.1.0\tools\psake.psm1 -force
+Import-Module $ThisDir\..\packages\psake.4.2.0.1\tools\psake.psm1 -force
 Invoke-psake $ThisDir\build-tasks.ps1 -properties  @{"MSBuildConfiguration"="Debug"}
 Remove-Module psake
