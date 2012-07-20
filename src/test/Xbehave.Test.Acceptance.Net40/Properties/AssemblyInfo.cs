c0d12c90bacf68fd50ca638756a60e16aab58ac0
Adam Ralph <adam@adamralph.com>
2012-07-20 21:19:34 +0200

    added acceptance test skeleton projects

---
+++ src/test/Xbehave.Test.Acceptance.Net40/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xbehave.Test.Acceptance")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]
