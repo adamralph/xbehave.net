30b57d72c5cdc7f68be028249bda65b7b84aca14
Adam Ralph <adam@adamralph.com>
2012-07-28 16:19:20 +0200

    improved BackgroundSample

--- src/Xbehave.Samples.Net40/BackgroundSample.cs
+++ src/Xbehave.Samples.Net40/BackgroundSample.cs
@@ -38,10 +38,10 @@ namespace Xbehave.Samples
                 .Given(() => Site.Login("Dr. Bill", "oranges"));
 
             "When I try to post to \"Expensive Therapy\""
-                .When(() => expensiveTherapy.Post(new Article()));
+                .When(() => expensiveTherapy.Post(new Article { Body = "This is a great blog!" }));
 
             "Then I should see \"Your article was published.\""
-                .Then(() => Site.CurrentPageText.Contains("Your article was published."));
+                .Then(() => Site.CurrentPage.Body.Contains("Your article was published."));
         }
 
         [Scenario]
@@ -51,10 +51,10 @@ namespace Xbehave.Samples
                 .Given(() => Site.Login("Dr. Bill", "oranges"));
 
             "When I try to post to \"Greg's anti-tax rants\""
-                .When(() => gregsAntiTaxRants.Post(new Article()));
+                .When(() => gregsAntiTaxRants.Post(new Article { Body = "This is a great blog!" }));
 
             "Then I should see \"Hey! That's not your blog!\""
-                .Then(() => Site.CurrentPageText.Contains("Hey! That's not your blog!"));
+                .Then(() => Site.CurrentPage.Body.Contains("Hey! That's not your blog!"));
         }
 
         [Scenario]
@@ -64,17 +64,17 @@ namespace Xbehave.Samples
                 .Given(() => Site.Login("Greg", "apples"));
 
             "When I try to post to \"Expensive Therapy\""
-                .When(() => expensiveTherapy.Post(new Article()));
+                .When(() => expensiveTherapy.Post(new Article { Body = "This is a great blog!" }));
 
             "Then I should see \"Your article was published.\""
-                .Then(() => Site.CurrentPageText.Contains("Your article was published."));
+                .Then(() => Site.CurrentPage.Body.Contains("Your article was published."));
         }
 
         private static class Site
         {
             public static User CurrentUser { get; private set; }
 
-            public static string CurrentPageText { get; set; }
+            public static Page CurrentPage { get; set; }
 
             public static void Login(string username, string password)
             {
@@ -92,6 +92,11 @@ namespace Xbehave.Samples
 
                 throw new System.InvalidOperationException("Invalid credentials.");
             }
+        }
+
+        private class Page
+        {
+            public string Body { get; set; }
         }
 
         private class User
@@ -115,17 +120,18 @@ namespace Xbehave.Samples
             {
                 if (Site.CurrentUser == this.Owner || Site.CurrentUser is GlobalAdministrator)
                 {
-                    Site.CurrentPageText = "Your article was published.";
+                    Site.CurrentPage = new Page { Body = "Your article was published.\n" + article.Body };
                 }
                 else
                 {
-                    Site.CurrentPageText = "Hey! That's not your blog!";
+                    Site.CurrentPage = new Page { Body = "Hey! That's not your blog!" };
                 }
             }
         }
 
         private class Article
         {
+            public string Body { get; set; }
         }
     }
 }
