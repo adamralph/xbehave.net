219231abc765631df69942cc56ab9a3ed61f4e44
Adam Ralph <adam@adamralph.com>
2012-07-31 08:21:09 +0200

    switched ExampleFeature to use TestRunner.Run() removed redundant TestRunner methods

--- src/test/Xbehave.Test.Acceptance.Net40/ExampleFeature.cs
+++ src/test/Xbehave.Test.Acceptance.Net40/ExampleFeature.cs
@@ -4,6 +4,7 @@
 
 namespace Xbehave.Test.Acceptance
 {
+    using System;
     using System.Collections.Concurrent;
     using System.Linq;
     using FluentAssertions;
@@ -20,32 +21,36 @@ namespace Xbehave.Test.Acceptance
         [Scenario]
         public static void ExecutionOfScenariosWithExamples()
         {
-            var scenario = default(IMethodInfo);
+            var feature = default(Type);
 
-            "Given a scenario with examples"
-                .Given(() => scenario = TestRunner.CreateScenario<int, int, int>(ScenarioWithASingleStepAndExamples));
+            "Given a feature with a scenario with examples"
+                .Given(() => feature = typeof(FeatureWithAScenarioWithASingleStepAndExamples));
 
-            "When the test runner executes the scenario"
-                .When(() => TestRunner.Execute(scenario));
+            "When the test runner runs the feature"
+                .When(() => TestRunner.Run(feature));
 
             "Then the scenario should be executed once for each example with the values from that example passed as arguments"
                 .Then(() =>
                 {
                     ArgumentLists.Select(arguments => arguments.Cast<int>()).OrderBy(x => x, new EnumerableComparer<int>())
                         .SequenceEqual(
-                            scenario.GetCustomAttributes(typeof(ExampleAttribute)).Select(x => x.GetInstance<ExampleAttribute>())
+                            Reflector.Wrap(feature.GetMethod("Scenario")).GetCustomAttributes(typeof(ExampleAttribute)).Select(x => x.GetInstance<ExampleAttribute>())
                                 .Select(example => example.DataValues.Cast<int>()).OrderBy(x => x, new EnumerableComparer<int>()),
                             new EnumerableEqualityComparer<int>()).Should().BeTrue();
                 });
         }
 
-        [Example(1, 2, 3)]
-        [Example(3, 4, 5)]
-        [Example(5, 6, 7)]
-        public static void ScenarioWithASingleStepAndExamples(int x, int y, int z)
+        private static class FeatureWithAScenarioWithASingleStepAndExamples
         {
-            "Given {0}, {1} and {2}"
-                .Given(() => ArgumentLists.Add(new object[] { x, y, z }));
+            [Scenario]
+            [Example(1, 2, 3)]
+            [Example(3, 4, 5)]
+            [Example(5, 6, 7)]
+            public static void Scenario(int x, int y, int z)
+            {
+                "Given {0}, {1} and {2}"
+                    .Given(() => ArgumentLists.Add(new object[] { x, y, z }));
+            }
         }
     }
 }
--- src/test/Xbehave.Test.Acceptance.Net40/Infrastructure/TestRunner.cs
+++ src/test/Xbehave.Test.Acceptance.Net40/Infrastructure/TestRunner.cs
@@ -17,48 +17,5 @@ namespace Xbehave.Test.Acceptance.Infrastructure
             return TestClassCommandRunner.Execute(feature, feature.EnumerateTestMethods().ToList(), startCallback: null, resultCallback: null).Results
                 .OfType<MethodResult>();
         }
-
-        public static IEnumerable<MethodResult> Execute(IMethodInfo scenario)
-        {
-            object feature = scenario.IsStatic ? null : scenario.CreateInstance();
-            return CreateSteps(scenario).Select(step => Execute(scenario, step, feature)).ToArray();
-        }
-
-        public static IEnumerable<ITestCommand> CreateSteps(IMethodInfo scenario)
-        {
-            return new ScenarioAttribute().CreateTestCommands(scenario);
-        }
-
-        public static IMethodInfo CreateScenario(Action action)
-        {
-            return Reflector.Wrap(action.Method);
-        }
-
-        public static IMethodInfo CreateScenario<T1>(Action<T1> action)
-        {
-            return Reflector.Wrap(action.Method);
-        }
-
-        public static IMethodInfo CreateScenario<T1, T2>(Action<T1, T2> action)
-        {
-            return Reflector.Wrap(action.Method);
-        }
-
-        public static IMethodInfo CreateScenario<T1, T2, T3>(Action<T1, T2, T3> action)
-        {
-            return Reflector.Wrap(action.Method);
-        }
-
-        private static MethodResult Execute(IMethodInfo method, ITestCommand step, object feature)
-        {
-            try
-            {
-                return step.Execute(feature);
-            }
-            catch (Exception ex)
-            {
-                return new FailedResult(method, ex, step.DisplayName);
-            }
-        }
     }
 }
