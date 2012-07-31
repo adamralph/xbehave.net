6c2000074cfbc4bf89acaaf633510ec404b8a49a
Adam Ralph <adam@adamralph.com>
2012-07-31 08:04:08 +0200

    switched InIsolationFeature to use TestRunner.Run()

--- src/test/Xbehave.Test.Acceptance.Net40/InIsolationFeature.cs
+++ src/test/Xbehave.Test.Acceptance.Net40/InIsolationFeature.cs
@@ -4,6 +4,7 @@
 
 namespace Xbehave.Test.Acceptance
 {
+    using System;
     using System.Linq;
     using FluentAssertions;
     using Xbehave.Test.Acceptance.Infrastructure;
@@ -17,42 +18,45 @@ namespace Xbehave.Test.Acceptance
         [Scenario]
         public static void UnfinishedFeature()
         {
-            var scenario = default(IMethodInfo);
+            var feature = default(Type);
             var results = default(MethodResult[]);
 
-            "Given a scenario with isolated steps which would cause following steps to fail if executed in the same context"
-                .Given(() => scenario = TestRunner.CreateScenario(ScenarioWithIsolatedStepsWhichWouldCauseFollowingStepsToFailIfExecutedInTheSameContext2));
+            "Given a feature with a scenario with isolated steps which would cause following steps to fail if executed in the same context"
+                .Given(() => feature = typeof(FeatureWithAScenarioWithIsolatedStepsWhichWouldCauseFollowingStepsToFailIfExecutedInTheSameContext));
 
-            "When the test runner executes the scenario"
-                .When(() => results = TestRunner.Execute(scenario).ToArray());
+            "When the test runner runs the scenario"
+                .When(() => results = TestRunner.Run(feature).ToArray());
 
             "Then there should be no failures"
                 .Then(() => results.Should().NotContain(result => result is FailedResult));
         }
 
-        public static void ScenarioWithIsolatedStepsWhichWouldCauseFollowingStepsToFailIfExecutedInTheSameContext2()
+        private static class FeatureWithAScenarioWithIsolatedStepsWhichWouldCauseFollowingStepsToFailIfExecutedInTheSameContext
         {
-            var stack = default(System.Collections.Generic.Stack<int>);
+            public static void Scenario()
+            {
+                var stack = default(System.Collections.Generic.Stack<int>);
 
-            "Given a stack"
-                .Given(() => stack = new System.Collections.Generic.Stack<int>());
+                "Given a stack"
+                    .Given(() => stack = new System.Collections.Generic.Stack<int>());
 
-            "When pushing 1 onto the stack"
-                .When(() => stack.Push(1));
+                "When pushing 1 onto the stack"
+                    .When(() => stack.Push(1));
 
-            "Then the first item in the stack should be 1"
-                .Then(() => stack.Pop().Should().Be(1))
-                .InIsolation();
+                "Then the first item in the stack should be 1"
+                    .Then(() => stack.Pop().Should().Be(1))
+                    .InIsolation();
 
-            "And the first item in the stack should be 1"
-                .And(() => stack.Pop().Should().Be(1))
-                .InIsolation();
+                "And the first item in the stack should be 1"
+                    .And(() => stack.Pop().Should().Be(1))
+                    .InIsolation();
 
-            "And the stack should contain some items"
-                .And(() => stack.Any());
+                "And the stack should contain some items"
+                    .And(() => stack.Any());
 
-            "But the stack should not contain more than one item"
-                .But(() => stack.Count.Should().BeLessOrEqualTo(1));
+                "But the stack should not contain more than one item"
+                    .But(() => stack.Count.Should().BeLessOrEqualTo(1));
+            }
         }
     }
 }
