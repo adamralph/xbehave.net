cba2c71c78b131986f196f6fc3011bd00de8e2bb
Adam Ralph <adam@adamralph.com>
2012-08-01 21:41:54 +0200

    marked internals as obsolete

--- src/Xbehave.Net40/Deprecated/Step.cs
+++ src/Xbehave.Net40/Deprecated/Step.cs
@@ -8,21 +8,25 @@ namespace Xbehave.Fluent
 
     internal partial class Step : IStep
     {
+        [Obsolete("Use When().Teardown() instead.")]
         public IStep When(string message, Action body, Action dispose)
         {
             return message.When(body).Teardown(dispose);
         }
 
+        [Obsolete("Use When().Teardown() instead.")]
         public IStep Then(string message, Action body, Action dispose)
         {
             return message.Then(body).Teardown(dispose);
         }
 
+        [Obsolete("Use When().Teardown() instead.")]
         public IStep And(string message, Action body, Action dispose)
         {
             return message.And(body).Teardown(dispose);
         }
 
+        [Obsolete("Use When().Teardown() instead.")]
         public IStep But(string message, Action body, Action dispose)
         {
             return message.But(body).Teardown(dispose);
