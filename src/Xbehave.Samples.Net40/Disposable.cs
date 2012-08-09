86967e984dd0effd6209be82ce387785164ed961
Adam Ralph <adam@adamralph.com>
2012-08-09 08:28:07 +0200

    re #25 added (rather contrived) TheLot sample to show all features combined

---
+++ src/Xbehave.Samples.Net40/Disposable.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Disposable.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Samples
+{
+    using System;
+
+    public sealed class Disposable : IDisposable
+    {
+        public Disposable()
+        {
+            Console.WriteLine("CREATED: {0}", this.GetHashCode());
+        }
+
+        public void Use()
+        {
+            Console.WriteLine("USED: {0}", this.GetHashCode());
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("DISPOSED: {0}", this.GetHashCode());
+        }
+    }
+}
---
+++ src/Xbehave.Samples.Net40/TheLot.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="TheLot.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Samples
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using Xbehave;
+
+    public class TheLot
+    {
+        private static Stack<int> stack;
+
+        [Background]
+        public static void Background()
+        {
+            "Given a stack"
+                .Given(() => stack = new Stack<int>());
+        }
+
+        [Scenario]
+        [Example(123)]
+        [Example(234)]
+        public static void Push(int element)
+        {
+            "Given {0}"
+                .Given(() => new Disposable().Using());
+
+            "When pushing {0} onto the stack"
+                .When(() => stack.Push(element))
+                .Teardown(() => stack.Clear())
+                .And()
+                .WithTimeout(1000);
+
+            "Then the stack should not be empty"
+                .Then(() => stack.Should().NotBeEmpty());
+
+            "And the stack pop should be {0}"
+                .And(() => stack.Pop().Should().Be(element))
+                .InIsolation()
+                .And()
+                .WithTimeout(1000);
+
+            "And the stack peek should be {0}"
+                .And(() => stack.Peek().Should().Be(element))
+                .WithTimeout(1000);
+
+            "And the stack count should be 3"
+                .And(() => stack.Count().Should().Be(2))
+                .Skip("because the assertion is nonsense");
+
+            "But the stack count should not be 2"
+                .But(() => stack.Count().Should().NotBe(2));
+        }
+    }
+}
