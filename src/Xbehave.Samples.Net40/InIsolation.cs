9ed4ac8eb629f7db2d7893efeb62561da6df45c0
aralph1@CHD06076002.ch.ad.hedani.net <aralph1@CHD06076002.ch.ad.hedani.net>
2012-08-10 12:37:00 +0200

    close #22 removed issues and added InIsolation sample

---
+++ src/Xbehave.Samples.Net40/InIsolation.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="InIsolation.cs" company="Adam Ralph">
+//  Copyright (c) Adam Ralph. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Samples
+{
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using Xbehave;
+
+    public class InIsolation
+    {
+        [Scenario]
+        public void Push()
+        {
+            var stack = default(Stack<int>);
+            var element = default(int);
+
+            "Given an element"
+                .Given(() => element = 11);
+
+            "And a stack"
+                .And(() => stack = new Stack<int>());
+
+            "When pushing the element onto the stack"
+                .When(() => stack.Push(element));
+
+            "Then the stack pop should be the element"
+                .Then(() => stack.Pop().Should().Be(element))
+                .InIsolation();
+
+            "And the stack should not be empty"
+                .And(() => stack.Should().NotBeEmpty());
+        }
+    }
+}
--- src/execute.issues.net40.bat
+++
@@ -1,3 +0,0 @@
-echo off
-..\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe "issues\Xbehave.Issues.Net40\bin\Debug\Xbehave.Issues.dll" /noshadow /nunit "issues\Xbehave.Issues.Net40\bin\Debug\Xbehave.Issues.Results.xml" /html "issues\Xbehave.Issues.Net40\bin\Debug\Xbehave.Issues.Results.html"
-pause
