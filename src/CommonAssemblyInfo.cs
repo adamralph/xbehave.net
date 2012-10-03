cd27a8caa88e5dddb7508eb48a392e79f44eeea2
adamralph <none@none>
2012-10-03 21:44:58 +0200

    increment to 0.13.0 for release

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.13.0.0")]
 [assembly: AssemblyFileVersion("0.13.0.0")]
-[assembly: AssemblyInformationalVersion("0.13.0-beta01")]
+[assembly: AssemblyInformationalVersion("0.13.0")]
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer in AssemblyInformationalVersion.")]
