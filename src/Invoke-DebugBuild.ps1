7f3017e93a5ec1d081f4a2424b03a3ea70e10227
Adam Ralph <adam@adamralph.com>
2012-11-17 13:53:00 +0100

    ensure build failures are surfaced

--- src/Invoke-DebugBuild.ps1
+++ src/Invoke-DebugBuild.ps1
@@ -1,4 +1,6 @@
 $ThisDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
 Import-Module $ThisDir\..\packages\psake.4.2.0.1\tools\psake.psm1 -force
 Invoke-psake $ThisDir\BuildTasks.ps1 -properties  @{"MSBuildConfiguration"="Debug"}
+$build_success = $psake.build_success
 Remove-Module psake
+if ($build_success -eq $false) { exit 1 } else { exit 0 }
