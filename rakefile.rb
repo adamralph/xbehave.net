1377904b83cb55ad1d44283d421dec77bcea886f
Adam Ralph <adam@adamralph.com>
2013-03-31 16:08:52 +0200

    re #33 moved rakefile.rb to root

--- rakefile.rb
+++ rakefile.rb
@@ -2,9 +2,9 @@ require 'albacore'
 require 'fileutils'
 require File.expand_path('rakehelper/rakehelper', File.dirname(__FILE__))
 
-ENV["XunitConsole_net20"] = "../packages/xunit.runners.1.9.1/tools/xunit.console.exe"
-ENV["XunitConsole_net40"] = "../packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
-ENV["NuGetConsole"] = "../packages/NuGet.CommandLine.2.2.0/tools/NuGet.exe"
+ENV["XunitConsole_net20"] = "packages/xunit.runners.1.9.1/tools/xunit.console.exe"
+ENV["XunitConsole_net40"] = "packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
+ENV["NuGetConsole"] = "packages/NuGet.CommandLine.2.2.0/tools/NuGet.exe"
 
 Albacore.configure do |config|
   config.log_level = :verbose
@@ -20,7 +20,7 @@ task :clean do
   build = RakeHelper.use_mono ? XBuild.new : MSBuild.new
   build.properties = { :configuration => :Release }
   build.targets = [ :Clean ]
-  build.solution = "XBehave.sln"
+  build.solution = "src/XBehave.sln"
   build.execute
 end
 
@@ -29,17 +29,17 @@ task :build do
   build = RakeHelper.use_mono ? XBuild.new : MSBuild.new
   build.properties = { :configuration => :Release }
   build.targets = [ :Build ]
-  build.solution = "XBehave.sln"
+  build.solution = "src/XBehave.sln"
   build.execute
 end
 
 desc "Execute specs"
 task :spec do
   specs = [
-    { :version => :net20, :path => "test/Xbehave.Sdk.Specifications.Net35/bin/Debug/Xbehave.Sdk.Specifications.Net35.dll" },
-    { :version => :net20, :path => "test/Xbehave.Specifications.Net35/bin/Debug/Xbehave.Specifications.Net35.dll" },
-    { :version => :net40, :path => "test/Xbehave.Sdk.Specifications.Net40/bin/Debug/Xbehave.Sdk.Specifications.Net40.dll" },
-    { :version => :net40, :path => "test/Xbehave.Specifications.Net40/bin/Debug/Xbehave.Specifications.Net40.dll" }
+    { :version => :net20, :path => "src/test/Xbehave.Sdk.Specifications.Net35/bin/Debug/Xbehave.Sdk.Specifications.Net35.dll" },
+    { :version => :net20, :path => "src/test/Xbehave.Specifications.Net35/bin/Debug/Xbehave.Specifications.Net35.dll" },
+    { :version => :net40, :path => "src/test/Xbehave.Sdk.Specifications.Net40/bin/Debug/Xbehave.Sdk.Specifications.Net40.dll" },
+    { :version => :net40, :path => "src/test/Xbehave.Specifications.Net40/bin/Debug/Xbehave.Specifications.Net40.dll" }
   ]
   execute specs
 end
@@ -47,8 +47,8 @@ end
 desc "Execute features"
 task :feature do
   features = [
-    { :version => :net20, :path => "test/Xbehave.Features.Net35/bin/Debug/Xbehave.Features.Net35.dll" },
-    { :version => :net40, :path => "test/Xbehave.Features.Net40/bin/Debug/Xbehave.Features.Net40.dll" }
+    { :version => :net20, :path => "src/test/Xbehave.Features.Net35/bin/Debug/Xbehave.Features.Net35.dll" },
+    { :version => :net40, :path => "src/test/Xbehave.Features.Net40/bin/Debug/Xbehave.Features.Net40.dll" }
   ]
   execute features
 end
@@ -59,15 +59,15 @@ nugetpack :nugetpack do |nuget|
   
   # NOTE (Adam): nuspec files can be consolidated after NuGet 2.3 is released - see http://nuget.codeplex.com/workitem/2767
   nuget.command = RakeHelper.nuget_command
-  nuget.nuspec = [ "Xbehave", ENV["OS"], "nuspec" ].select { |token| token }.join(".")  
+  nuget.nuspec = [ "src/Xbehave", ENV["OS"], "nuspec" ].select { |token| token }.join(".")  
   nuget.output = "bin"
 end
 
 desc "Execute samples"
 task :sample do
   samples = [
-    { :version => :net20, :path => "Xbehave.Samples.Net35/bin/Debug/Xbehave.Samples.Net35.dll" },
-    { :version => :net40, :path => "Xbehave.Samples.Net40/bin/Debug/Xbehave.Samples.Net40.dll" }
+    { :version => :net20, :path => "src/Xbehave.Samples.Net35/bin/Debug/Xbehave.Samples.Net35.dll" },
+    { :version => :net40, :path => "src/Xbehave.Samples.Net40/bin/Debug/Xbehave.Samples.Net40.dll" }
   ]
   execute samples
 end
