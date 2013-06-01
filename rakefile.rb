74b62b48b998470f8adc35895af9c4798bf23804
Adam Ralph <adam@adamralph.com>
2013-06-02 01:55:01 +0200

    #59 extraction of version from CommonAssemblyInfo.cs and passing to nuget as param

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,7 @@
 require 'albacore'
 require 'fileutils'
 
+version = IO.read("src/CommonAssemblyInfo.cs").split(/AssemblyInformationalVersion\("/, 2)[1].split(/"/).first
 xunit_command_net20 = "packages/xunit.runners.1.9.1/tools/xunit.console.exe"
 xunit_command_net40 = "packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
 nuget_command = "packages/NuGet.CommandLine.2.2.0/tools/NuGet.exe"
@@ -59,11 +60,10 @@ task :feature => [:build] do
 end
 
 desc "Create the nuget package"
-nugetpack :pack => [:build] do |nuget|
+exec :pack => [:build] do |cmd|
   FileUtils.mkpath output
-  nuget.command = nuget_command
-  nuget.nuspec = nuspec
-  nuget.output = output
+  cmd.command = nuget_command
+  cmd.parameters "pack " + nuspec + " -Version " + version + " -OutputDirectory " + output
 end
 
 desc "Execute samples"
