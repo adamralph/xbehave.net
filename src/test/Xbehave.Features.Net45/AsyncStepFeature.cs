6421ba551c950bec6cad2203228e428cc059cb67
Adam Ralph <adam@adamralph.com>
2014-01-26 15:46:38 +0100

    #25 refactor: aligned AsyncStepFeature scenario style, refactored some scenarios and removed redundant code

--- src/test/Xbehave.Features.Net45/AsyncStepFeature.cs
+++ src/test/Xbehave.Features.Net45/AsyncStepFeature.cs
@@ -6,250 +6,212 @@ namespace Xbehave.Features
 {
     using System;
     using System.Linq;
-    using System.Threading;
     using System.Threading.Tasks;
-
     using FluentAssertions;
-    
     using Xbehave;
     using Xbehave.Test.Acceptance.Infrastructure;
     using Xunit.Sdk;
 
     public class AsyncStepFeature
     {
-        private static readonly ManualResetEventSlim @Event = new ManualResetEventSlim();
-
-        // used for async void method group test
-        private static bool asyncVoidSet = false;
-
         [Scenario]
-        public void AsyncStepExecutesToCompletionBeforeNextStep(bool set)
+        public void AsyncStep(bool asyncStepHasCompleted)
         {
-            "Given a boolean set to false"._(() => set = false);
-
-            "When it is set to true inside an asynchronous step"._(async () =>
+            "When an async step is executed"._(async () =>
             {
                 await Task.Delay(500);
-                set = true;
+                asyncStepHasCompleted = true;
             });
 
-            "Then its value is true when the next step begins"._(() =>
+            "Then it is completed before the next step is executed"._(() =>
+                asyncStepHasCompleted.Should().BeTrue());
+        }
+
+        [Scenario]
+        public void AllMethodsAreUsedAsync(int count)
+        {
+            "Given the count is 20".Given(async () =>
             {
-                set.Should().BeTrue();
+                await Task.Yield();
+                count = 20;
+            });
+
+            "When it is increased by one".When(async () =>
+            {
+                await Task.Yield();
+                count++;
+            });
+
+            "And it is increased by two using the underscore method"._(async () =>
+            {
+                await Task.Yield();
+                count += 2;
+            });
+
+            "And it is increased by two using the f method".f(async () =>
+            {
+                await Task.Yield();
+                count += 2;
+            });
+
+            "Then it is 25"._(async () =>
+            {
+                await Task.Yield();
+                count.Should().Be(25);
+            });
+
+            "And obviously it is greater than 10".And(async () =>
+            {
+                await Task.Yield();
+                count.Should().BeGreaterThan(10);
+            });
+
+            "But evidently it is not 24".But(async () =>
+            {
+                await Task.Yield();
+                count.Should().NotBe(24);
             });
         }
 
         [Scenario]
-        public void AsyncSupportForAllMethods(int calls)
+        public void MultipleAsyncSteps(int number)
         {
-            calls = 0;
-
-            "Given a variable initialized to 20".Given(async () => 
+            "Given a number initialised as 2"._(async () =>
             {
-                await Task.Yield();
-                calls = 20;
+                await Task.Delay(100);
+                number = 2;
             });
 
-            "When it is increased by one".When(async () => 
+            "When it is incremented in an asynchronous step"._(async () =>
             {
-                await Task.Yield();
-                calls++;
+                await Task.Delay(100);
+                number++;
             });
 
-            "And increased by four".And(async () => 
+            "And it is incremented again in another asynchronous step"._(async () =>
             {
-                await Task.Yield();
-                calls += 4;
+                await Task.Delay(100);
+                number++;
             });
 
-            "Then the resulting value should be 25".Then(async () =>
-            {
-                await Task.Yield();
-                calls.Should().Be(25);
-            });
-
-            "And obviously is greater than 10".f(async () =>
-            {
-                await Task.Yield();
-                calls.Should().BeGreaterThan(10);
-            });
-
-            "But evidently not 24".But(async () =>
-            {
-                await Task.Yield();
-                calls.Should().NotBe(24);
-            });
+            "Then it is 4"._(() =>
+                number.Should().Be(4));
         }
 
         [Scenario]
-        public void MultipleAsyncStepExecuteToCompletionBeforeNextStep(int first, int second, int third)
+        public void AsyncStepDoesNotTimeout(bool asyncStepHasCompleted)
         {
-            "Given three numbers initialized in 2"._(async () =>
-            {
-                await Task.Delay(100);
-                first = 2;
-                second = 2;
-                third = 2;
-            });
-
-            "When all of them are incremented in an asynchronous step"._(async () =>
-            {
-                await Task.Delay(100);
-                first++;
-                second++;
-                third++;
-            });
-
-            "And all of them are incremented in an asynchronous step again"._(async () =>
-            {
-                await Task.Delay(100);
-                first++;
-                second++;
-                third++;
-            });
-
-            "Then all values are 4"._(() =>
-            {
-                first.Should().Be(4);
-                first.Should().Be(4);
-                first.Should().Be(4);
-            });
-        }
-
-        [Scenario]
-        public void AsyncStepThatExecutesBeforeTimeoutExpiresIsCompleted(bool set)
-        {
-            "Given a boolean set to false"._(() => set = false);
-
-            "When it is set to true inside an asynchronous step"._(async () =>
+            "When an asynchronous step with a timeout which does not timeout is executed"._(async () =>
             {
                 await Task.Delay(500);
-                set = true;
-            }).WithTimeout(1000);
+                asyncStepHasCompleted = true;
+            })
+            .WithTimeout(1000);
 
-            "Then its value is true when the next step begins"._(() =>
-            {
-                set.Should().BeTrue();
-            });
+            "Then it has completed before the next step begins"._(() =>
+                asyncStepHasCompleted.Should().BeTrue());
         }
 
         [Scenario]
-        public void AsyncStepWithTaskReturnTypeThrowsException(Exception e)
+        public void AsyncTaskStepThrowsException(Type feature, MethodResult[] results, Exception e)
         {
-            var feature = default(Type);
-            var results = default(MethodResult[]);
-
-            "Given a feature with a scenario that throws an InvalidOperationException"._(() => feature = typeof(TaskReturningAsyncStepThrowsException));
+            "Given a feature with a scenario that throws an InvalidOperationException"._(() =>
+                feature = typeof(AsyncTaskStepWhichThrowsException));
 
             "When the test runner runs the feature"._(() =>
-                {
-                    results = TestRunner.Run(feature).ToArray();
-                });
+                results = TestRunner.Run(feature).ToArray());
 
-            "Then the result should be a failure"
-                .And(() => results.Should().ContainItemsAssignableTo<FailedResult>());
+            "Then the result should be a failure"._(() =>
+                results.Should().ContainItemsAssignableTo<FailedResult>());
 
-            "And the exception type should be \"System.InvalidOperationException\""
-                .And(() =>
-                {
-                    var failedResult = results.Cast<FailedResult>().Single();
-
-                    failedResult.ExceptionType.Should().Be("System.InvalidOperationException");
-                });
+            "And the exception type should be \"System.InvalidOperationException\"".And(() =>
+                results.Cast<FailedResult>().Single().ExceptionType.Should().Be("System.InvalidOperationException"));
         }
 
         [Scenario]
-        public void AsyncStepWithVoidReturnTypeThrowsException(Exception e)
+        public void AsyncVoidStepThrowsException(Type feature, MethodResult[] results, Exception e)
         {
-            var feature = default(Type);
-            var results = default(MethodResult[]);
-
-            "Given a feature with a scenario that throws an InvalidOperationException"._(() => feature = typeof(VoidReturningAsyncStepThrowsException));
+            "Given a feature with a scenario that throws an InvalidOperationException"._(() =>
+                feature = typeof(AsyncVoidStepWhichThrowsException));
 
             "When the test runner runs the feature"._(() =>
-                {
-                    results = TestRunner.Run(feature).ToArray();
-                });
+                results = TestRunner.Run(feature).ToArray());
 
-            "Then the result should be a failure"
-                .And(() => results.Should().ContainItemsAssignableTo<FailedResult>());
+            "Then the result should be a failure"._(() =>
+                results.Should().ContainItemsAssignableTo<FailedResult>());
 
-            "And the exception type should be \"System.InvalidOperationException\""
-                .And(() =>
-                {
-                    var failedResult = results.Cast<FailedResult>().First();
-
-                    failedResult.ExceptionType.Should().Be("System.InvalidOperationException");
-                });
+            "And the exception type should be \"System.InvalidOperationException\""._(() =>
+                results.Cast<FailedResult>().First().ExceptionType.Should().Be("System.InvalidOperationException"));
         }
 
         [Scenario]
-        public void StepExecutesTooSlowly()
+        public void AsyncStepExceedsTimeout(Type feature, MethodResult[] results)
         {
-            var feature = default(Type);
-            var results = default(MethodResult[]);
+            "Given a feature with a scenario with a single step which exceeds it's 1ms timeout"._(() =>
+                feature = typeof(AsyncStepWhichExceedsTimeout));
 
-            "Given a feature with a scenario with a single step which exceeds it's 1ms timeout"
-                .Given(() => feature = typeof(StepTooSlow));
+            "When the test runner runs the feature"._(() =>
+                results = TestRunner.Run(feature).ToArray());
 
-            "When the test runner runs the feature"
-                .When(() =>
-                {
-                    @Event.Reset();
-                    results = TestRunner.Run(feature).ToArray();
-                })
-                .Teardown(() => @Event.Set());
+            "Then there should be one result"._(() =>
+                results.Count().Should().Be(1));
 
-            "Then there should be one result"
-                .Then(() => results.Count().Should().Be(1));
+            "And the result should be a failure"._(() =>
+                results.Should().ContainItemsAssignableTo<FailedResult>());
 
-            "And the result should be a failure"
-                .And(() => results.Should().ContainItemsAssignableTo<FailedResult>());
-
-            "And the result message should be \"Test execution time exceeded: 1ms\""
-                .And(() => results.Cast<FailedResult>().Should().OnlyContain(result => result.Message == "Test execution time exceeded: 1ms"));
+            "And the result message should be \"Test execution time exceeded: 1ms\""._(() =>
+                results.Cast<FailedResult>().Should().OnlyContain(result => result.Message == "Test execution time exceeded: 1ms"));
         }
 
         [Scenario]
-        public void AsyncStepExecutesToCompletionBeforeNextStepIfStepReturnsVoidNotTask(bool set)
+        public void ExecutingAnAsyncVoidStepUsingMethodGroupSyntax()
         {
-            "Given a boolean set to false"._(() => asyncVoidSet = false);
+            "When an async void method is executed in a step using method group syntax"._(
+                (Action)AsyncVoidMethodType.AsyncVoidMethod);
 
-            "When it is set to true inside an asynchronous step"._((Action)this.AsyncVoidStep);
+            "Then the method has completed before the next step begins"._(() =>
+                AsyncVoidMethodType.AsyncVoidMethodHasCompleted.Should().BeTrue());
+        }
 
-            "Then its value is true when the next step begins"._(() =>
+        private static class AsyncVoidMethodType
+        {
+            private static bool asyncVoidMethodHasCompleted;
+
+            public static bool AsyncVoidMethodHasCompleted
             {
-                asyncVoidSet.Should().BeTrue();
-            });
+                get { return AsyncVoidMethodType.asyncVoidMethodHasCompleted; }
+            }
+
+            public static async void AsyncVoidMethod()
+            {
+                asyncVoidMethodHasCompleted = false;
+                await Task.Delay(500);
+                asyncVoidMethodHasCompleted = true;
+            }
         }
 
-        private async void AsyncVoidStep()
-        {
-            await Task.Delay(500);
-            asyncVoidSet = true;
-        }
-
-        private class TaskReturningAsyncStepThrowsException
+        private class AsyncTaskStepWhichThrowsException
         {
             [Scenario]
             public void Scenario()
             {
                 // disabling warning about async method not having await. it's intended
-                #pragma warning disable 1998
-                "Given something"._(async () => 
-                { 
-                    throw new InvalidOperationException(); 
+#pragma warning disable 1998
+                "Given something"._(async () =>
+                {
+                    throw new InvalidOperationException();
                 });
-                #pragma warning restore 1998
+#pragma warning restore 1998
             }
         }
 
-        private class VoidReturningAsyncStepThrowsException
+        private class AsyncVoidStepWhichThrowsException
         {
             [Scenario]
             public void Scenario()
             {
-                "Given something"._((Action)this.Step);
+                "Given something"._(
+                    (Action)this.Step);
             }
 
             private async void Step()
@@ -259,12 +221,13 @@ namespace Xbehave.Features
             }
         }
 
-        private class StepTooSlow
+        private class AsyncStepWhichExceedsTimeout
         {
             [Scenario]
             public void Scenario(bool set)
             {
-                "Given something"._(async () => await Task.Delay(500)).WithTimeout(1);
+                "Given something"._(async () =>
+                    await Task.Delay(500)).WithTimeout(1);
             }
         }
     }
