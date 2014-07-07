a068dc5a2b6a70e3f104ce329a248e75f93d6888
Adam Ralph <adam@adamralph.com>
2014-07-07 19:12:36 +0200

    refactor: boil down StepTestCaseRunner

--- src/Xbehave.2.Execution/StepTestCaseRunner.cs
+++ src/Xbehave.2.Execution/StepTestCaseRunner.cs
@@ -7,6 +7,7 @@ namespace Xbehave.Execution
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Xunit.Abstractions;
     using Xunit.Sdk;
 
     public class StepTestCaseRunner : TestCaseRunner<StepTestCase>
@@ -25,38 +26,34 @@ namespace Xbehave.Execution
             var summary = new RunSummary { Total = 1 };
             var timer = new ExecutionTimer();
 
-            if (!MessageBus.QueueMessage(new TestStarting(TestCase, TestCase.DisplayName)))
-            {
-                CancellationTokenSource.Cancel();
-            }
-            else
+            if (this.Queue(new TestStarting(TestCase, TestCase.DisplayName)))
             {
                 try
                 {
                     await timer.AggregateAsync(() => TestCase.Step.RunAsync());
-
-                    if (!MessageBus.QueueMessage(new TestPassed(TestCase, TestCase.DisplayName, timer.Total, null)))
-                    {
-                        CancellationTokenSource.Cancel();
-                    }
+                    this.Queue(new TestPassed(TestCase, TestCase.DisplayName, timer.Total, null));
                 }
                 catch (Exception ex)
                 {
                     summary.Failed++;
-                    if (!MessageBus.QueueMessage(new TestFailed(TestCase, TestCase.DisplayName, timer.Total, null, ex)))
-                    {
-                        CancellationTokenSource.Cancel();
-                    }
+                    this.Queue(new TestFailed(TestCase, TestCase.DisplayName, timer.Total, null, ex));
                 }
             }
 
-            if (!MessageBus.QueueMessage(new TestFinished(TestCase, TestCase.DisplayName, timer.Total, null)))
-            {
-                CancellationTokenSource.Cancel();
-            }
-
+            this.Queue(new TestFinished(TestCase, TestCase.DisplayName, timer.Total, null));
             summary.Time = timer.Total;
             return summary;
+        }
+
+        private bool Queue(IMessageSinkMessage message)
+        {
+            if (!MessageBus.QueueMessage(message))
+            {
+                CancellationTokenSource.Cancel();
+                return false;
+            }
+
+            return true;
         }
     }
 }
