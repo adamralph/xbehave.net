d1cab82f8f2c52bda39574c6a40db6fc051d3f99
Adam Ralph <adam@adamralph.com>
2014-11-23 17:32:30 +0100

    red: added AsyncStepFeature to v2

--- src/test/Xbehave.Test.Acceptance.Net45/AsyncStepFeature.cs
+++ src/test/Xbehave.Test.Acceptance.Net45/AsyncStepFeature.cs
@@ -2,7 +2,6 @@
 //  Copyright (c) xBehave.net contributors. All rights reserved.
 // </copyright>
 
-#if !V2
 namespace Xbehave.Test.Acceptance
 {
     using System;
@@ -264,4 +263,3 @@ namespace Xbehave.Test.Acceptance
         }
     }
 }
-#endif
