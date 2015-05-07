ad8d1487e79bc98683f9a295e0fc8060a901824b
Adam Ralph <adam@adamralph.com>
2015-05-07 22:25:12 +0200

    red: add StepDefinitionFilterFeature

---
+++ src/Xbehave.2.Core/Sdk/IFilter.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IFilter.cs" company="xBehave.net contributors">
+//  Copyright (c) xBehave.net contributors. All rights reserved.
+// </copyright>
+
+namespace Xbehave.Sdk
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Filters a list of <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of items contained in a list.</typeparam>
+    public interface IFilter<T>
+    {
+        /// <summary>
+        /// Filters a list of <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="source">The list to filter.</param>
+        /// <returns>The filtered list.</returns>
+        IEnumerable<T> Filter(IEnumerable<T> source);
+    }
+}
