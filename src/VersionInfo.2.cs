aa07b42acbe15436823024d22f11347f82c0aaaf
Adam Ralph <adam@adamralph.com>
2015-08-06 18:44:30 +0200

    build: increment to 2.1.0

--- src/VersionInfo.2.cs
+++ src/VersionInfo.2.cs
@@ -1,9 +1,9 @@
-﻿// <copyright file="VersionInfo.2.cs" company="xBehave.net contributors">
+// <copyright file="VersionInfo.2.cs" company="xBehave.net contributors">
 //  Copyright (c) xBehave.net contributors. All rights reserved.
 // </copyright>
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
