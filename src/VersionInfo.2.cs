1b3a8d5fd9330dfee4f9d4be05a0c9dd4196823c
Adam Ralph <adam@adamralph.com>
2016-10-11 13:46:59 +0200

    support TestOutputHelper when invoking step

--- src/VersionInfo.2.cs
+++ src/VersionInfo.2.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.1")]
