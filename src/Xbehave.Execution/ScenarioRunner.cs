d7ff8e152595de9ded92cc52b59bd5b84760d908
Adam Ralph <adam@adamralph.com>
2018-12-23 22:00:35 +0000

    wip

--- src/Xbehave.Execution/ScenarioRunner.cs
+++ src/Xbehave.Execution/ScenarioRunner.cs
@@ -1,106 +1,89 @@
+// https://github.com/xunit/xunit/blob/2.4.1/src/xunit.execution/Sdk/Frameworks/Runners/XunitTestRunner.cs
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Xbehave.Sdk;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
 namespace Xbehave.Execution
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Xbehave.Execution.Extensions;
-    using Xbehave.Sdk;
-    using Xunit.Sdk;
-
-    public class ScenarioRunner
+    /// <summary>
+    /// The test runner for xUnit.net v2 tests.
+    /// </summary>
+    public class ScenarioRunner : ScenarioRunnerBase<IXunitTestCase>
     {
-        private readonly IScenario scenario;
-        private readonly IMessageBus messageBus;
-        private readonly Type scenarioClass;
-        private readonly object[] constructorArguments;
-        private readonly MethodInfo scenarioMethod;
-        private readonly object[] scenarioMethodArguments;
-        private readonly string skipReason;
-        private readonly IReadOnlyList<BeforeAfterTestAttribute> beforeAfterScenarioAttributes;
-        private readonly ExceptionAggregator parentAggregator;
-        private readonly CancellationTokenSource cancellationTokenSource;
+        readonly IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes;
 
-        public ScenarioRunner(
-            IScenario scenario,
-            IMessageBus messageBus,
-            Type scenarioClass,
-            object[] constructorArguments,
-            MethodInfo scenarioMethod,
-            object[] scenarioMethodArguments,
-            string skipReason,
-            IReadOnlyList<BeforeAfterTestAttribute> beforeAfterScenarioAttributes,
-            ExceptionAggregator aggregator,
-            CancellationTokenSource cancellationTokenSource)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScenarioRunner"/> class.
+        /// </summary>
+        /// <param name="test">The test that this invocation belongs to.</param>
+        /// <param name="messageBus">The message bus to report run status to.</param>
+        /// <param name="testClass">The test class that the test method belongs to.</param>
+        /// <param name="constructorArguments">The arguments to be passed to the test class constructor.</param>
+        /// <param name="testMethod">The test method that will be invoked.</param>
+        /// <param name="testMethodArguments">The arguments to be passed to the test method.</param>
+        /// <param name="skipReason">The skip reason, if the test is to be skipped.</param>
+        /// <param name="beforeAfterAttributes">The list of <see cref="BeforeAfterTestAttribute"/>s for this test.</param>
+        /// <param name="aggregator">The exception aggregator used to run code and collect exceptions.</param>
+        /// <param name="cancellationTokenSource">The task cancellation token source, used to cancel the test run.</param>
+        public ScenarioRunner(IScenario test,
+                               IMessageBus messageBus,
+                               Type testClass,
+                               object[] constructorArguments,
+                               MethodInfo testMethod,
+                               object[] testMethodArguments,
+                               string skipReason,
+                               IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes,
+                               ExceptionAggregator aggregator,
+                               CancellationTokenSource cancellationTokenSource)
+            : base(test, messageBus, testClass, constructorArguments, testMethod, testMethodArguments, skipReason, aggregator, cancellationTokenSource)
         {
-            Guard.AgainstNullArgument(nameof(scenario), scenario);
-            Guard.AgainstNullArgument(nameof(messageBus), messageBus);
-            Guard.AgainstNullArgument(nameof(aggregator), aggregator);
-
-            this.scenario = scenario;
-            this.messageBus = messageBus;
-            this.scenarioClass = scenarioClass;
-            this.constructorArguments = constructorArguments;
-            this.scenarioMethod = scenarioMethod;
-            this.scenarioMethodArguments = scenarioMethodArguments;
-            this.skipReason = skipReason;
-            this.beforeAfterScenarioAttributes = beforeAfterScenarioAttributes;
-            this.parentAggregator = aggregator;
-            this.cancellationTokenSource = cancellationTokenSource;
+            this.beforeAfterAttributes = beforeAfterAttributes;
         }
 
-        public async Task<RunSummary> RunAsync()
+        /// <summary>
+        /// Gets the list of <see cref="BeforeAfterTestAttribute"/>s for this test.
+        /// </summary>
+        protected IReadOnlyList<BeforeAfterTestAttribute> BeforeAfterAttributes
+            => beforeAfterAttributes;
+
+        /// <inheritdoc/>
+        protected override async Task<Tuple<RunSummary, string>> InvokeTestAsync(ExceptionAggregator aggregator)
         {
-            if (!string.IsNullOrEmpty(this.skipReason))
+            var output = string.Empty;
+
+            TestOutputHelper testOutputHelper = null;
+            foreach (object obj in ConstructorArguments)
             {
-                this.messageBus.Queue(
-                    this.scenario, test => new TestSkipped(test, this.skipReason), this.cancellationTokenSource);
-
-                return new RunSummary { Total = 1, Skipped = 1 };
+                testOutputHelper = obj as TestOutputHelper;
+                if (testOutputHelper != null)
+                    break;
             }
-            else
+
+            if (testOutputHelper != null)
+                testOutputHelper.Initialize(MessageBus, Test);
+
+            var executionTime = await InvokeTestMethodAsync(aggregator);
+
+            if (testOutputHelper != null)
             {
-                var summary = new RunSummary();
-                var output = string.Empty;
-                var childAggregator = new ExceptionAggregator(this.parentAggregator);
-                if (!childAggregator.HasExceptions)
-                {
-                    summary.Aggregate(await childAggregator.RunAsync(() => this.InvokeScenarioAsync(childAggregator)));
-                }
-
-                var exception = childAggregator.ToException();
-                if (exception != null)
-                {
-                    summary.Total++;
-                    summary.Failed++;
-                    this.messageBus.Queue(
-                        this.scenario,
-                        test => new TestFailed(test, summary.Time, string.Empty, exception),
-                        this.cancellationTokenSource);
-                }
-                else if (summary.Total == 0)
-                {
-                    summary.Total++;
-                    this.messageBus.Queue(
-                        this.scenario, test => new TestPassed(test, summary.Time, string.Empty), this.cancellationTokenSource);
-                }
-
-                return summary;
+                output = testOutputHelper.Output;
+                testOutputHelper.Uninitialize();
             }
+
+            return Tuple.Create(executionTime, output);
         }
 
-        private async Task<RunSummary> InvokeScenarioAsync(ExceptionAggregator aggregator) =>
-            await new ScenarioInvoker(
-                    this.scenario,
-                    this.messageBus,
-                    this.scenarioClass,
-                    this.constructorArguments,
-                    this.scenarioMethod,
-                    this.scenarioMethodArguments,
-                    this.beforeAfterScenarioAttributes,
-                    aggregator,
-                    this.cancellationTokenSource)
-                .RunAsync();
+        /// <summary>
+        /// Override this method to invoke the test method.
+        /// </summary>
+        /// <param name="aggregator">The exception aggregator used to run code and collect exceptions.</param>
+        /// <returns>Returns the execution time (in seconds) spent running the test method.</returns>
+        protected virtual Task<RunSummary> InvokeTestMethodAsync(ExceptionAggregator aggregator)
+            => new ScenarioInvoker(Test, MessageBus, TestClass, ConstructorArguments, TestMethod, TestMethodArguments, BeforeAfterAttributes, aggregator, CancellationTokenSource).RunAsync();
     }
 }
