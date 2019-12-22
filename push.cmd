e30bcbd5f6a5d1dfc7e6a9c1f87122e109322452
Adam Ralph <adam@adamralph.com>
2019-12-22 12:55:35 +0000

    wip

--- push.cmd
+++ push.cmd
@@ -1,7 +1,7 @@
 @echo Off
 
 if [%MYGET_ADAMRALPH_CI_API_KEY%] == [] (
-  echo "%~nx0: MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s)."
+  echo:%~nx0: MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s).
 ) else (
   forfiles /m *.nupkg /s /c "cmd /c echo %~nx0: Pushing @path && dotnet nuget push @path --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key %MYGET_ADAMRALPH_CI_API_KEY% }}"
 )
